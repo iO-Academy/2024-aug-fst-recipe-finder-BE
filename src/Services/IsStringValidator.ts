8459a2c913add426d2344c34e91970d90ff665fd
Finn <finley.champion@gmail.com>
2024-12-10 14:03:05 +0000

    added tests for email

--- src/Services/IsStringValidator.ts
+++ src/Services/IsStringValidator.ts
@@ -1,7 +1,7 @@
-function isString(input) {
+function isString(input: string): boolean {
   if (typeof input === "string") {
     return true;
-  }
+  } else return false
 }
 
 module.exports = { isString };
--- src/tests/ValidationServices.test.ts
+++ src/tests/ValidationServices.test.ts
@@ -3,20 +3,24 @@ import {describe, expect, test} from '@jest/globals';
 
 const email = require('../Services/EmailValidator');
 const string = require('../Services/IsStringValidator');
+const length = require('../Services/StringLenghtValidator');
 
-
-
-// test('string is valid'), () => {
-//     const actual = ;
-//     const expected = true;
-//     expect(actual).toBe(expected);
-
-// }
-
-test('string is valid', () => {
+test('input is a string', () => {
     expect(string.isString('string')).toBe(true);
+})
+
+test('input isnt a string', () => {
+    expect(string.isString(1)).toBe(false);
 })
 
 test('email is valid', () => {
     expect(email.emailValidator('you@me.com')).toBe(true);
+})
+
+test('email has incorrect characters', () => {
+    expect(email.emailValidator('you@@me.com')).toBe(false);
+})
+
+test('string is a correct length', () => {
+    expect(length.validateLength('heeellloooooo')).toBe(true);
 })
