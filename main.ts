c1feb72e71858aa95bc07a6b8c9e35cbd1673208
chris <landwaterfood@gmail.com>
2024-12-11 15:36:00 +0000

    added routes

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import { getUser } from './src/Controllers/userController';
 import { addRecipe } from './src/Controllers/addRecipeController';
 import { getAllUserRecipes } from './src/Controllers/getAllUserRecipesController';
 import { getRecipeById } from './src/Controllers/getRecipeByIdController';
+import { getAllIngredients } from './src/Controllers/getAllIngredientsController';
 
 const app = express();
 const port = 3002;
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.post("/users", getUser)
 app.post("/users/:userId/recipes", addRecipe)
+app.get('/users/:userId/ingredients', getAllIngredients)
 app.get('/users/:userId/recipes', getAllUserRecipes)
 app.get('/users/:userId/recipes/:recipeId', getRecipeById)
 
