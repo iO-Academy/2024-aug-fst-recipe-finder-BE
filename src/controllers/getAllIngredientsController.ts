a3b930974d80f4667c39275f3d10c1a2cefba719
chris <landwaterfood@gmail.com>
2024-12-11 15:13:45 +0000

    created controller sql error codes

---
+++ src/controllers/getAllIngredientsController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import getDatabase from "../Services/databaseConnector";
+import { userIdExists } from "../Services/validators";
+import { Connection } from "promise-mysql";
+
+export async function getAllIngredients(req: Request, res: Response): Promise<void> {
+  try {
+    const db: Connection = await getDatabase();
+    const userId: number = Number(req.params.userId);
+
+    if (await userIdExists(db, userId)) {
+      const ingredients: [{id: number, name: string, duration: number}] = await db.query(
+        "SELECT `id`, `name` FROM `ingredients` WHERE `user_id` = ?",
+        [userId]
+      );
+      res.status(200).json({
+        message: "Successfully retrieved all ingredients",
+        data: ingredients,
+      });
+    } else {
+      res.status(400).json({
+        message: "Invalid user id",
+        data: []
+      });
+    }
+  } catch {
+    res.status(500).json({
+      message: "Unexpected error",
+      data: []
+    });
+  }
+}
