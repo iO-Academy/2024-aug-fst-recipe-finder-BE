ff65eee22453832915df323d6a72860ab268e79a
chris <landwaterfood@gmail.com>
2024-12-11 16:30:55 +0000

    working

--- src/controllers/getSingleIngredientController.ts
+++ src/controllers/getSingleIngredientController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import getDatabase from "../Services/databaseConnector";
-import { userIdExists } from "../Services/validators";
+import { ingredientIdExists, userIdExists } from "../Services/validators";
 import { Connection } from "promise-mysql";
 
 export async function getSingleIngredient(req: Request, res: Response): Promise<void> {
@@ -10,22 +10,30 @@ export async function getSingleIngredient(req: Request, res: Response): Promise<
     const ingredientId: number = Number(req.params.ingredientId);
 
 
-    if (await userIdExists(db, userId)) {
-      const ingredient: [{id: number, name: string}] = await db.query(
-        "SELECT `id`, `name` FROM `ingredients` WHERE `user_id` = ? AND `id` = ?",
-        [userId, ingredientId]
-      );
-      res.status(200).json({
-        message: "Successfully retrieved ingredient",
-        data: ingredient,
-      });
-    } else {
+    if (await ingredientIdExists (db, ingredientId)) {
+        if (await userIdExists(db, userId))  {
+        const ingredient: [{id: number, name: string}] = await db.query(
+            "SELECT `id`, `name` FROM `ingredients` WHERE `user_id` = ? AND `id` = ?",
+            [userId, ingredientId]
+        );
+        res.status(200).json({
+            message: "Successfully retrieved ingredient",
+            data: ingredient,
+        });
+        } else { 
+        res.status(400).json({
+            message: "Invalid user id",
+            data: []
+        });
+    }
+    }else { 
       res.status(400).json({
-        message: "Invalid user id",
+        message: "Invalid ingredient id",
         data: []
       });
+
     }
-  } catch {
+} catch {
     res.status(500).json({
       message: "Unexpected error",
       data: []
