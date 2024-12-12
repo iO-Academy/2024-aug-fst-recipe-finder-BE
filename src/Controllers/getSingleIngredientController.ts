b359f172d3cf17fb25d6c21437beab2ff82a4d59
Finn <finley.champion@gmail.com>
2024-12-12 10:31:52 +0000

    added controller changes to SIcontroller

--- src/Controllers/getSingleIngredientController.ts
+++ src/Controllers/getSingleIngredientController.ts
@@ -3,40 +3,43 @@ import getDatabase from "../Services/databaseConnector";
 import { ingredientIdExists, userIdExists } from "../Services/validators";
 import { Connection } from "promise-mysql";
 
-export async function getSingleIngredient(req: Request, res: Response): Promise<void> {
+export async function getSingleIngredient(
+  req: Request,
+  res: Response
+): Promise<void> {
   try {
     const db: Connection = await getDatabase();
     const userId: number = Number(req.params.userId);
     const ingredientId: number = Number(req.params.ingredientId);
 
-
-    if (await ingredientIdExists (db, ingredientId)) {
-        if (await userIdExists(db, userId))  {
-        const ingredient: [{id: number, name: string}] = await db.query(
-            "SELECT `id`, `name` FROM `ingredients` WHERE `user_id` = ? AND `id` = ?",
-            [userId, ingredientId]
-        );
-        res.status(200).json({
-            message: "Successfully retrieved ingredient",
-            data: ingredient,
-        });
-        } else { 
-        res.status(400).json({
-            message: "Invalid user id",
-            data: []
-        });
+      if (!(await userIdExists (db, userId))) {
+      res.status(400).json({
+        message: "Invalid user id",
+        data: [],
+      });
+      return
     }
-    }else { 
+    
+    if (!(await ingredientIdExists(db, ingredientId))) {
       res.status(400).json({
         message: "Invalid ingredient id",
-        data: []
+        data: [],
       });
-
+      return;
     }
-} catch {
+    
+    const ingredient: [{ id: number; name: string }] = await db.query(
+      "SELECT `id`, `name` FROM `ingredients` WHERE `user_id` = ? AND `id` = ?",
+      [userId, ingredientId]
+    );
+    res.status(200).json({
+      message: "Successfully retrieved ingredient",
+      data: ingredient,
+    });
+  } catch {
     res.status(500).json({
       message: "Unexpected error",
-      data: []
+      data: [],
     });
   }
 }
