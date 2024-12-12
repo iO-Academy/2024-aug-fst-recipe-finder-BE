9b34cd1c0c258d0c8e1d0fea95d01940d2635363
Liam O'Connell <liamslost@googlemail.com>
2024-12-12 10:09:37 +0000

    add ingredient controller working and tested

--- src/Controllers/addIngredientController.ts
+++ src/Controllers/addIngredientController.ts
@@ -8,58 +8,40 @@ import {
 import { Connection } from "promise-mysql";
 
 export async function addIngredient(req: Request, res: Response) {
-
   try {
     const db: Connection = await getDatabase();
     const userId: number = Number(req.params.userId);
 
-    if(!isIdValid(userId) || !(await userIdExists(db, userId))) {
+    if (!isIdValid(userId) || !(await userIdExists(db, userId))) {
       res.status(400).json({
         message: "Invalid user id",
       });
-      return
+      return;
     }
 
-    if(
-      !stringLengthIsValid(req.body.name, 1, 254)
-    ) {
+    if (!stringLengthIsValid(req.body.name, 1, 254)) {
       res.status(400).json({
         message: "Invalid data",
       });
-      return
+      return;
     }
 
     const results = await db.query(
-      "INSERT INTO `ingredients` SET ?",
-        {
-          user_id: userId,
-          name: req.body.name,
-          instructions: req.body.instructions,
-          prep_time: req.body.prep_time,
-          cook_time: req.body.cook_time
-      }
+      "INSERT INTO `ingredients` ( name ) VALUES (?)",
+      [req.body.name]
     );
-    const newRecipeId = results.insertId;
-
-    let inputs = validIngredients.map((ingredient) => {
-      return [newRecipeId, ingredient]
-    })
-
-    await db.query(
-      "INSERT INTO `recipes_ingredients` (recipe_id, ingredient_id) VALUES ?",
-      [inputs]
-    );
+    const newIngredientId = results.insertId;
 
     res.status(201).json({
-      message: "Successfully created recipe",
+      message: "Successfully added ingredient",
       data: {
-        recipeId: newRecipeId,
-      }
-    })
-
-  } catch {
+        id: newIngredientId,
+      },
+    });
+  } catch (error) {
     res.status(500).json({
-      message: "Unexpected error"
+      message: "Unexpected error",
+      data: error.toString(),
     });
   }
 }
