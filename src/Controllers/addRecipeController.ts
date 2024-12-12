191008647f5a174d0b6768a11403bb585d16928a
Liam O'Connell <liamslost@googlemail.com>
2024-12-12 14:32:16 +0000

    trainer edits v6

--- src/Controllers/addRecipeController.ts
+++ src/Controllers/addRecipeController.ts
@@ -3,7 +3,7 @@ import getDatabase from "../Services/databaseConnector";
 import {
   ingredientIdExists,
   isIdValid,
-  numberIsLessThanMaxAllowed,
+  numberInRange,
   stringLengthIsValid,
   userIdExists,
 } from "../Services/validators";
@@ -30,8 +30,8 @@ export async function addRecipe(req: Request, res: Response) {
     if (
       !stringLengthIsValid(req.body.name, 1, 254) ||
       !stringLengthIsValid(req.body.instructions, 1, 65534) ||
-      !numberIsLessThanMaxAllowed(req.body.prep_time) ||
-      !numberIsLessThanMaxAllowed(req.body.cook_time) ||
+      !numberInRange(req.body.prep_time) ||
+      !numberInRange(req.body.cook_time) ||
       validIngredients.length < req.body.ingredients.length
     ) {
       res.status(400).json({
