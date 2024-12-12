174088ae9f9e3eddd1e48192cb57b6934ec4ca12
Liam O'Connell <liamslost@googlemail.com>
2024-12-12 10:59:43 +0000

    hot fix to remove hard coded recipe id on get recipes by id and edit ingredients type annotations to array

--- src/Controllers/getRecipeByIdController.ts
+++ src/Controllers/getRecipeByIdController.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express";
 import getDatabase from "../Services/databaseConnector";
-import { userIdExists, recipeIdExists, isIdValid } from "../Services/validators";
+import {
+  userIdExists,
+  recipeIdExists,
+  isIdValid,
+} from "../Services/validators";
 import { Connection } from "promise-mysql";
 
-interface Ingredient {id: number, name: string}
+interface Ingredient {
+  id: number;
+  name: string;
+}
 interface Recipe {
-  id: number,
-  name: string,
-  instructions: string,
-  prep_time: number,
-  cook_time: number,
-  ingredients: Ingredient[]
+  id: number;
+  name: string;
+  instructions: string;
+  prep_time: number;
+  cook_time: number;
+  ingredients: Ingredient[];
 }
 
 export async function getRecipeById(
@@ -22,44 +29,45 @@ export async function getRecipeById(
     const userId: number = Number(req.params.userId);
     const recipeId: number = Number(req.params.recipeId);
 
-    if(!isIdValid(userId) || !(await userIdExists(db, userId))) {
+    if (!isIdValid(userId) || !(await userIdExists(db, userId))) {
       res.status(400).json({
         message: "Invalid user id",
       });
-      return
+      return;
     }
 
-    if(!isIdValid(recipeId) || !(await recipeIdExists(db, recipeId))) {
+    if (!isIdValid(recipeId) || !(await recipeIdExists(db, recipeId))) {
       res.status(400).json({
         message: "Invalid recipe id",
       });
-      return
+      return;
     }
 
     let recipe: Recipe = await db.query(
       "SELECT `id`, `name`, `instructions`, `prep_time`, `cook_time` FROM `recipes` WHERE `id` = ?",
       [recipeId]
     );
-    recipe = recipe[0]
+    recipe = recipe[0];
 
-    const ingredients: [Ingredient] = await db.query(`
+    const ingredients: Ingredient[] = await db.query(
+      `
       SELECT ingredients.id, ingredients.name
       FROM ingredients
       JOIN recipes_ingredients
       ON ingredients.id = recipes_ingredients.ingredient_id
-      WHERE recipes_ingredients.recipe_id = 1;
-    `);
-    recipe.ingredients = ingredients
+      WHERE recipes_ingredients.recipe_id = ?;
+    `, [recipeId]
+    );
+    recipe.ingredients = ingredients;
 
     res.status(200).json({
       message: "Successfully retrieved recipe",
-      data: recipe
+      data: recipe,
     });
-
-  } catch {
+  } catch (error){
     res.status(500).json({
       message: "Unexpected error",
-      data: [],
+      data: [], error: error.toString()
     });
   }
 }
